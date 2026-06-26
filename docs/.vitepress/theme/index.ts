6f302085c21925ada83233d2656dfbe1668eb659
rodrigo.arenas <31422766+rodrigo-arenas@users.noreply.github.com>
2026-06-25 20:40:27 -0500

    docs: remove unused EditorLayout wireframe component
    Replaced by the editor-full.png screenshot in the previous session.
    The component was registered globally but not referenced in any .md file.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -7,7 +7,6 @@ import './styles/custom.css'
 
 import FlowPipeline from './components/FlowPipeline.vue'
 import NodeCategoryGrid from './components/NodeCategoryGrid.vue'
-import EditorLayout from './components/EditorLayout.vue'
 import DataTransform from './components/DataTransform.vue'
 import ForkJoin from './components/ForkJoin.vue'
 import ScheduleCycle from './components/ScheduleCycle.vue'
@@ -22,7 +21,6 @@ export default {
   enhanceApp({ app }) {
     app.component('FlowPipeline', FlowPipeline)
     app.component('NodeCategoryGrid', NodeCategoryGrid)
-    app.component('EditorLayout', EditorLayout)
     app.component('DataTransform', DataTransform)
     app.component('ForkJoin', ForkJoin)
     app.component('ScheduleCycle', ScheduleCycle)
